caba53ff367e022dee0562abdd6efd0e8768c166
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2021-12-29 11:05:40 +0800

    lesson 52

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -4,23 +4,28 @@ import axios from "axios";
 import defaultAvatar from '../../assets/defaultAvatar.png'
 
 export function Users(props: UsersPropsType) {
-  if (props.usersPage.users.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      return response.data.items
-    }).then(users => {
-      console.log(users)
-      props.setUsers(users)
-    })
+  const getUsers = () => {
+    if (props.usersPage.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        return response.data.items
+      }).then(users => {
+        console.log(users)
+        props.setUsers(users)
+      })
+    }
   }
+
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {
         props.usersPage.users.map(m => {
           return (
             <div key={m.id}>
               <span>
-                <div>
-                  <img src={m.photos.small ? m.photos.small: defaultAvatar} alt="avatar"/>
+                <div >
+                  <img src={m.photos.small ? m.photos.small: defaultAvatar} alt="avatar"
+                       style={{width: "50px", height: "50px", borderRadius: "50%"}}/>
                 </div>
                 <div>
                   {m.followed ? <button onClick={()=> props.unfollow(m.id)}>Unfollow</button>
